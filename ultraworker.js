ff831365581af5e8c012dc2aed3bca6fff89914e
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-02 11:20:18 -0700

    asd

--- ultraworker.js
+++ ultraworker.js
@@ -1,30 +1,32 @@
-importScripts("https://cdn.jsdelivr.net/gh/Bromine-Labs/cdn/uv/uv.bundle.js");
-importScripts("uv.config.js");
-importScripts("/dist/uv.sw.js");
-importScripts("/scram/scramjet.shared.js", "/scram/scramjet.worker.js");
+importScripts("https://brominecdn.netlify.app/uv/uv.bundle.js")
+importScripts("uv.config.js")
+importScripts(__uv$config.sw)
+importScripts("/scram/scramjet.shared.js", "/scram/scramjet.worker.js")
 
 if (navigator.userAgent.includes("Firefox")) {
   Object.defineProperty(globalThis, "crossOriginIsolated", {
     value: true,
     writable: true,
-  });
+  })
 }
 
-const uv = new UVServiceWorker();
-const scramjet = new ScramjetServiceWorker();
+const uv = new UVServiceWorker()
+const scramjet = new ScramjetServiceWorker()
 
 self.addEventListener("install", () => {
-  self.skipWaiting();
-});
+  self.skipWaiting()
+})
 
-const handleReq = async (event) => {
-  await scramjet.loadConfig();
+async function handleRequest(event) {
+  await scramjet.loadConfig()
 
-  if (scramjet.route(event)) return scramjet.fetch(event);
+  if (scramjet.route(event)) return scramjet.fetch(event)
 
-  if (uv.route(event)) return await uv.fetch(event);
+  if (uv.route(event)) return await uv.fetch(event)
 
-  return await fetch(event.request);
-};
+  return await fetch(event.request)
+}
 
-self.addEventListener("fetch", (e) => e.respondWith(handleReq(e)));
+self.addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event))
+})
