6dc111b1d4abe24f84d46279e70c32bfa42d9d4e
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-03 10:32:00 -0700

    push for a sec

--- ultraworker.js
+++ ultraworker.js
@@ -1,4 +1,4 @@
-importScripts("https://brominecdn.netlify.app/uv/uv.bundle.js")
+importScripts("https://unpkg.com/@titaniumnetwork-dev/ultraviolet/dist/uv.bundle.js")
 importScripts("uv.config.js")
 importScripts(__uv$config.sw)
 importScripts("/scram/scramjet.shared.js", "/scram/scramjet.worker.js")
